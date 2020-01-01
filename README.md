8d21b949b5cc6f45038ddf8ef5014aab31f31246
impshum <impshum@impshum.co.uk>
2020-01-01 15:34:02 +0000

    up we go

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# autoflair
- 
+![](https://recycledrobot.co.uk/reddit_git.jpg)
+
+## AutoFlair
+
+Streams Reddit, counts submissions and assigns different flairs depending on score.
+
+![](ss.jpg)
+
+### Features
+
+-   Streams Reddit and applies flairs in real-time
+-   SQLite database to store all the juicy data
+-   Simple flair system setup
+-   Does not eat the cat
+
+### Instructions
+
+-   Install requirements `pip install -r requirements.txt`
+-   Create Reddit (script) app at <https://www.reddit.com/prefs/apps/> and get keys
+-   Edit conf.ini with your details
+-   Run it `python run.py`
+
+### Settings Info
+
+-   `target_subreddit` - Subreddit to target
+-   `points_per_post` - Points to award for each new submission
+-   `test_mode` - Run the script without changing the database or flairs
+
+### Flair Setup Instructions
+
+In the config.ini file you should see the code below. Each flair level is assigned here.
+
+    [1]
+    text = one
+    class = one
+
+    [3]
+    text = three
+    class = three
+
+    [10]
+    text = ten
+    class = ten
+
+The titles of these sections eg `[1]` and `[3]` are the minimum points needed to be awarded that flair.
+
+The items under the section title are as follows
+
+-   `text` - The flair text
+-   `class` - The flair class to be applied (see CSS section below)
+
+### CSS flair class
+
+We declare the `class` here as `.flair-whatever`
+
+So...
+
+    [10]
+    text = SQUIRREL
+    class = squirrel
+
+Would be
+
+    .flair-squirrel {
+        background-color: #19572a;
+        color: #fff;
+        padding: 2px 19px;
+        font-weight: 600;
+    }
+
+Edit to your liking and add it to your CSS stylesheet. You can use the same class for all flairs if you like.
---
+++ run.py
@@ -0,0 +1,128 @@
+import time
+import praw
+import sqlite3
+from sqlite3 import Error
+import configparser
+
+
+class C:
+    W, G, R, P, Y, C = '\033[0m', '\033[92m', '\033[91m', '\033[95m', '\033[93m', '\033[36m'
+
+
+def db_connect():
+    try:
+        conn = sqlite3.connect('data.db')
+        create_table(conn)
+        return conn
+    except Error as e:
+        print(e)
+    return None
+
+
+def create_table(conn):
+    create_table = """CREATE TABLE IF NOT EXISTS autoflair (
+                                        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                                        author TEXT NOT NULL,
+                                        subreddit TEXT NOT NULL,
+                                        level INT NOT NULL
+                                        );"""
+    conn.execute(create_table)
+
+
+def insert_row(conn, author, subreddit, test_mode, level):
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT * FROM autoflair WHERE author = ? AND subreddit = ?", (author, subreddit))
+    query = cur.fetchall()
+    if not len(query):
+        conn.execute(
+            "INSERT INTO autoflair (author, subreddit, level) VALUES (?, ?, ?);", (author, subreddit, level))
+    else:
+        x = cur.execute(
+            "UPDATE autoflair SET level = level + ? WHERE author = ? AND subreddit = ?", (level, author, subreddit))
+        level = query[0][3] + level
+
+    if not test_mode:
+        conn.commit()
+    return str(level)
+
+
+def read_db(conn):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM autoflair")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+
+def sql_init():
+    conn = db_connect()
+    # create_table(conn)
+    #insert_row(conn, 'impshum', 'python', False, 1)
+    read_db(conn)
+    #search_db(conn, 'author', 'impshum')
+
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('conf.ini')
+    target_subreddit = config['SETTINGS']['target_subreddit']
+    points_per_post = int(config['SETTINGS']['points_per_post'])
+    test_mode = config['SETTINGS'].getboolean('test_mode')
+
+    tm = ''
+    if test_mode:
+        tm = f'{C.R}TEST MODE{C.Y}'
+
+    print(f"""{C.Y}
+╔═╗╦ ╦╔╦╗╔═╗╔═╗╦  ╔═╗╦╦═╗
+╠═╣║ ║ ║ ║ ║╠╣ ║  ╠═╣║╠╦╝ {tm}
+╩ ╩╚═╝ ╩ ╚═╝╚  ╩═╝╩ ╩╩╩╚═ {C.C}v1.0 {C.G}impshum{C.W}
+    """)
+
+    reddit = praw.Reddit(
+        username=config['REDDIT']['reddit_user'],
+        password=config['REDDIT']['reddit_pass'],
+        client_id=config['REDDIT']['client_id'],
+        client_secret=config['REDDIT']['client_secret'],
+        user_agent='Autoflair (by u/impshum)'
+    )
+
+    flairs = {}
+    skip_sections = {'REDDIT', 'SETTINGS'}
+    sections = [e for e in config.sections() if e not in skip_sections]
+    for section in sections:
+        flair_text = config[section]['text']
+        flair_class = config[section]['class']
+        flairs.update(
+            {str(section): {'text': flair_text, 'class': flair_class}})
+
+    conn = db_connect()
+    start_time = time.time()
+    sub = reddit.subreddit(target_subreddit)
+
+    for submission in sub.stream.submissions():
+        if submission.created_utc > start_time:
+            author = submission.author.name
+            subreddit = submission.subreddit
+            saved = submission.saved
+            c = C.Y
+            f_level = ''
+
+            if not saved:
+                new_level = insert_row(conn, str(author), str(subreddit), test_mode, 1)
+                f_level = f'p/{new_level}'
+                if new_level in flairs:
+                    c = C.G
+                    if not test_mode:
+                        flair_text = flairs[new_level]['text']
+                        flair_class = flairs[new_level]['class']
+                        sub.flair.set(author, flair_text, css_class=flair_class)
+                        submission.save()
+
+            print(f'{c}u/{author} r/{subreddit} {f_level}{C.W}')
+
+
+if __name__ == '__main__':
+    main()
+    #sql_init()
